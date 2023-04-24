b9084d688a7778ff235891064e765587f657ee5b
RodCato <code@eliment.org>
2023-04-24 18:00:34 -0500

    Second driver switch

--- high-order-crod-will.js
+++ high-order-crod-will.js
@@ -22,22 +22,37 @@ console.log(multiplesOfTen);
 // Write a function that takes in an array of numbers and returns a new array with only odd numbers.
 
 // Pseudocode:
-// input:
-// output:
-// function name:
-// expected output:
+// input: array of numbers 
+// output: array with only odd numbers
+// function name: oddNums
+    //iterate across all values of the array
+    //check to see if the value is odd
+    //return array with only odd values
+// expected output: [ 7, 3, 5 ,13 ]
 
-// const arr2 = [2, 7, 3, 5, 8, 10, 13]
+const arr2 = [2, 7, 3, 5, 8, 10, 13]
+
+const oddNums = arr2.filter((value) => value % 2 != 0)
+
+console.log(oddNums)
+// output: [ 7, 3, 5, 13 ]
 
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use the typeof operator.
 
 // Pseudocode:
-// input:
-// output:
-// function name:
-// expected output:
+// input: array of numbers and letters
+// output: string 
+// function name: lettersLeft
+    //iterate across all values of the array
+    //check to see if value is a character
+    // return string of those characters
+// expected output: "nice work"
 
-// const comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+const comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+
+const lettersLeft = comboArr.filter((value) => typeof value === "string")
+
+console.log(lettersLeft.join(""))
 
 // Create a function that takes in an array and returns an array without any false, null, 0, or blank values.
 
