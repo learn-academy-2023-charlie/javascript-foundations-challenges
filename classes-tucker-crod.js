694c1dc313eb9f10795dccf9e96373741dae3736
Tmacintyre <Tucker.w.macintyre@gmail.com>
2023-04-27 18:17:27 -0400

    switching drivers

--- classes-tucker-crod.js
+++ classes-tucker-crod.js
@@ -18,35 +18,81 @@
 //console.log(blackCoffee.coffeeProfile())
     //output: A black coffee with 1 cream, 2 sugars
     
-class Coffee {
-  constructor(type, cream, sugar) {
-    this.type = type.toLowerCase()
-    this.cream = cream
-    this.sugar = sugar
-  }
+// class Coffee {
+//   constructor(type, cream, sugar) {
+//     this.type = type.toLowerCase()
+//     this.cream = cream
+//     this.sugar = sugar
+//   }
 
-  coffeeProfile() {
-    return `A ${this.type} coffee with ${this.creams()}, ${this.sugars()}`
-  }
+//   coffeeProfile() {
+//     return `A ${this.type} coffee with ${this.creams()}, ${this.sugars()}`
+//   }
 
-  creams() {
-    if (this.cream > 1) {
-      return `${this.cream} creams`
-    } else {
-      return `${this.cream} cream`
-    }
-  }
+//   creams() {
+//     if (this.cream > 1) {
+//       return `${this.cream} creams`
+//     } else {
+//       return `${this.cream} cream`
+//     }
+//   }
 
-  sugars() {
-    if (this.sugar > 1) {
-      return `${this.sugar} sugars`
-    } else {
-      return `${this.sugar} sugar`
-    }
-  }
-}
-const blackCoffee = new Coffee("black", 1, 2)
-   // output: Coffee { type: 'black', cream: 2, sugar: 2 }
+//   sugars() {
+//     if (this.sugar > 1) {
+//       return `${this.sugar} sugars`
+//     } else {
+//       return `${this.sugar} sugar`
+//     }
+//   }
+// }
+// const blackCoffee = new Coffee("black", 1, 2)
+//    output: Coffee { type: 'black', cream: 2, sugar: 2 }
 
-console.log(blackCoffee.coffeeProfile())
-console.log(blackCoffee)
+// console.log(blackCoffee.coffeeProfile())
+// console.log(blackCoffee)
+
+
+
+// Latte Maker: create a class for Latte
+
+// class Latte {
+//   constructor(flavor, milktype, shots){
+//     this.flavor = flavor.toLowerCase()
+//     this.milktype = milktype
+//     this.shots = shots
+//     } 
+//   latteProfile(){
+//     return `A ${this.flavor} latte with ${this.milktype} milk, ${this.shot()}.`
+//   }
+//   shot(){
+//     if(this.shots > 1){
+//       return `${this.shots} shots`
+//     } else{
+//       return `${this.shots} shot`
+//     }
+//   }
+// }
+
+// const latteType = new Latte("hazelnut","almond" , 2)
+// console.log(latteType.latteProfile())
+
+
+// Write a Latte class that takes a flavor, a milk type, and a number of shots
+// Write a method for your Latte class that outputs the latte's profile
+// Write the code that makes a regular, single shot latte
+  // Output: const latteType = new Latte("regular","no" , 1)
+
+// Log the regular, single shot latte's profile
+  //console.log(latteType.latteProfile())
+
+// Write the code that makes a double shot, hazelnut latte with almond milk.
+  // Output: const latteType = new Latte("hazelnut","almond" , 2)
+
+// Log the double shot, hazelnut latte with almond milk's profile.
+  // Output: A hazelnut latte with almond milk, 2 shots.
+
+
+// Volume of a Cylinder: create a class for Cylinder
+// Write a class that calculates the volume of a Cylinder: v = πr^2h (r is the radius and h is the height of the cylinder)
+// Write the code that rounds the volume of the cylinder to four decimal places
+// Write the code that creates three unique cylinder objects
